e994c10365d6177b533fc1ed2932adcc8b82f721
SaturnKai <55664707+SaturnKai@users.noreply.github.com>
2023-10-27 16:57:25 -0700

    add routes and dynamic nav

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,5 +1,43 @@
-<header>
-	<img src="/logo.svg" alt="logo" />
+<script lang="ts">
+	export let page: string = '';
+</script>
+
+<header class="header">
+	<img class="logo" src="/logo.svg" alt="logo" />
+	<nav class="nav">
+		<a class="tab" class:selected={page === '/'} href="/">HOME</a>
+		<a class="tab" class:selected={page === '/menu'} href="/menu">MENU</a>
+		<a class="tab" class:selected={page === '/locations'} href="/locations">LOCATIONS</a>
+		<a class="tab" class:selected={page === '/contact'} href="/contact">CONTACT</a>
+	</nav>
 </header>
 
-<nav />
+<style>
+	.header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin-top: 20px;
+		margin-inline: 180px;
+	}
+
+	.logo {
+		width: 100px;
+	}
+
+	.tab {
+		font-size: 1.2rem;
+		font-weight: bold;
+		color: #fff;
+		text-decoration: none;
+		margin-left: 20px;
+	}
+
+	.tab:hover {
+		color: #a1a1a1;
+	}
+
+	.tab.selected {
+		color: #eb703b;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import Header from 'components/Header.svelte';
 	import Footer from 'components/Footer.svelte';
+	import { page } from '$app/stores';
 
 	import 'styles/main.css';
 </script>
 
-<Header />
+<Header page={$page.url.pathname} />
 <main>
 	<slot />
 </main>
