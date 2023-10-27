a27265219c5dcad6b3143f7320e58a7281674338
Ohlin Arellano <ohlin.arellano@gmail.com>
2023-10-27 16:58:41 -0700

    Added footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    export let icon: string;
+</script>
+
+{#if icon}
+<div class="icon-splash">
+    {icon}
+</div>
+{/if}
+
+<footer>
+    <div></div>
+    <div class="facts">
+        <h1>Did you know?</h1>
+        <span>Boba is super dope and cool</span>
+    </div>
+    <div class="socials">
+        Instagram
+    </div>
+</footer>
+
+<style>
+    footer {
+        padding: 1.5rem;
+        width: 100%;
+
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+
+        background-color: #28252d;
+    }
+
+    .facts {
+        
+    }
+</style>
