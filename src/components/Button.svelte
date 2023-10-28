2f45241fab23e5cf58e51c6b31cb9722938e9ef9
SaturnKai <55664707+SaturnKai@users.noreply.github.com>
2023-10-28 00:35:38 -0700

    add button component

---
+++ src/components/Button.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	export let color: string;
+	export let shadow: string;
+	export let hover: string;
+	export let icon = '';
+</script>
+
+<button class="button" style="--color: {color}; --shadow: {shadow}; --hover: {hover}" on:click>
+	<slot />
+	{#if icon.length}
+		<i class="fa-solid fa-{icon}" />
+	{/if}
+</button>
+
+<style>
+	.button {
+		font-size: 1rem;
+		font-weight: bold;
+		color: #fff;
+		text-decoration: none;
+		background: var(--color);
+		border-radius: 8px;
+		padding: 12px 20px;
+		box-shadow: 4px 0px 30px 5px var(--shadow);
+		transition: background 80ms ease;
+		outline: none;
+		border: none;
+	}
+
+	.button:hover {
+		background: var(--hover);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
+<script lang="ts">
+	import Button from 'components/Button.svelte';
+</script>
+
 <h1>Home</h1>
+<Button color="#ff5151" shadow="#ff3a3a55" hover="#e44747">Menu</Button>
+<Button color="#8951FF" shadow="#623AFF55" hover="#703fd9" icon="paper-plane">Send</Button>
