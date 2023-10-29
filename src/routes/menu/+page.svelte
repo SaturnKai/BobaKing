424c8d3403de0b9a4de405ca88dd67677fc2f72a
Ohlin Arellano <ohlin.arellano@gmail.com>
2023-10-29 01:42:12 -0700

    Menu V1 complete, needs toppings and more drink versions

--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Separator from 'components/Separator.svelte';
+	import PriceTable from 'components/PriceTable.svelte';
 	import menu from '$lib/menu';
 </script>
 
@@ -13,6 +14,11 @@
 	<p class="description">
 		Your choice of teas, slushes, and toppings, for <span class="text-green">2,000+</span> combinations.
 	</p>
+
+	<div class="price-section">
+		<PriceTable />
+	</div>
+
 	<div class="menu-section">
 		<img src="/assets/img/milktea.webp" alt="Thai Milk Tea" />
 		<div class="menu-listing">
@@ -25,7 +31,7 @@
 				{/each}
 			</div>
 		</div>
-		<div class="menu-listing">
+		<div class="menu-listing" style="border-color: #393042;">
 			<div class="menu-head">
 				<h1>{menu[1].name}</h1>
 			</div>
@@ -36,6 +42,23 @@
 			</div>
 		</div>
 	</div>
+	
+	<div class="menu-section">
+		<img src="/assets/img/slush.webp" alt="Thai Milk Tea" />
+		<div class="menu-listing">
+			<div class="menu-head">
+				<h1>{menu[2].name}</h1>
+				<h1 style="padding: 0 1rem 0 1rem;color:#393042;">/</h1>
+				<h1 class="separated"> Milk Blend</h1>
+			</div>
+			<div class="menu-items">
+				{#each menu[2].flavors as item}
+					<p>{item}</p>
+				{/each}
+			</div>
+		</div>
+	</div>
+
 </div>
 
 <style>
@@ -64,12 +87,20 @@
 		color: #2dc86f;
 	}
 
+	.price-section {
+		display: flex;
+		width: 100%;
+		align-items: center;
+		justify-content: center;
+		padding: 4rem;
+	}
+
 	.menu-section {
 		display: flex;
 		flex-direction: row;
 		justify-content: flex-start;
 		padding: 2rem;
-		height: 20rem;
+		height: 22.5rem;
 	}
 
 	.menu-head h1 {
@@ -77,17 +108,23 @@
 		color: #ffb186;
 	}
 
+	.menu-head {
+		display: flex;
+		padding-bottom: 0.5rem;
+	}
+
 	.menu-listing {
 		width: 100%;
 		padding-left: 2rem;
+		border-left: 0.25rem solid transparent;
 	}
 
 	.menu-items {
 		display: flex;
 		flex-direction: column;
 		flex-wrap: wrap;
-		gap: 0.25rem;
-		height: 100%;
+		gap: 0.40rem;
+		height: calc(100% - 2rem);
 		color: #fff;
 		font-weight: 600;
 		padding: 0.5rem;
