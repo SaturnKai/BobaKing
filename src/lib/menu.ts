bc0453caca4eda842df934b16ec2f29c690eb320
SaturnKai <55664707+SaturnKai@users.noreply.github.com>
2023-10-29 00:39:37 -0700

    add menu lib

--- src/lib/menu.ts
+++ src/lib/menu.ts
@@ -1,6 +1,21 @@
-
-const menu = {
-    "Milk Tea": {
-        
-    }
+interface ItemType {
+	name: string;
+	item_columns: string[][];
 }
+
+const menu: ItemType[] = [
+	{
+		name: 'Milk Tea',
+		item_columns: [['Thai', 'Black', 'Jasmine', 'Green', 'Earl Grey', 'Moroccan', 'Camomile']]
+	},
+	{
+		name: 'Flavored',
+		item_columns: [
+			['Mango', 'Passion Fruit', 'Pomegranate', 'Pistachio', 'Strawberry', 'Almond', 'Coffee'],
+			['Chocolate', 'Matcha Green Tea', 'Honeyew', 'Taro', 'Peach', 'Lychee', 'Wintermelon'],
+			['Hokkaido', 'Nagasaki', 'Horchata', 'Rose', 'Coconut', 'Mix and Match']
+		]
+	}
+];
+
+export default menu;
